a327271ce024aa1f61766079a85bff581d0d4e21
briangm98nevermore <briangm980227@gmail.com>
2024-10-26 17:38:37 -0400

    añadiendo prefijo /game y modificando controlador para API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\ToroVacaGameController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('game')->group(function () {
+
+Route::get('/tiempodejuego/{time}',[ToroVacaGameController::class,'timergame']);
+Route::get('/ruta1',[ToroVacaGameController::class,'index1']);
+Route::get('/ruta2',[ToroVacaGameController::class,'index2']);
+Route::get('/ruta3',[ToroVacaGameController::class,'index3']);
+
+});

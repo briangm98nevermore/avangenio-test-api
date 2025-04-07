28e090eabebb5ca77f2cee3c7d724b44eff2298a
briangm98nevermore <briangm980227@gmail.com>
2025-04-07 00:52:07 -0400

    Modificando home view

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/main', function () {
+    return 'Esto es un mensaje de Brian González XD';
+});
+
 
 Route::get('/send-message', function () {
-    $chatId = '817775788'; // Replace with your chat ID
-    $message = 'Te amo mucho...Nunca lo olvidessssssssssssssssss';
+    $chatId = '925445465'; // Replace with your chat ID
+    $message = 'this is a test message';
 
     Telegram::sendMessage([
     'chat_id' => $chatId,
